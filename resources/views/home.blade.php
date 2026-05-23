12fba67d602ff54e967c13f704def611005c704c
Mizume25 <gabrielnivicela649@gmail.com>
2026-05-23 17:35:43 +0200

    Controladores inciales, y primeras rutas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('adminlte::page') {{-- Plantilla de AdminLTE--}}
+
+@section('title', 'Productos') {{-- Titulo --}}
+
+@section('content_header') {{-- Header --}}
+    <h1>Productos</h1>
+@endsection
+
+@section('content') {{-- Contenido --}}
+
+@endsection
+
+
+
